d7a5533ad56844804ae91b3449ce4c1b1b777726
Susanna Mahlamäki <liisa.mahlamaki@helsinki.fi>
2022-11-20 18:22:31 +0200

    luokkien luomista

---
+++ src/repositories/user_repository.py
@@ -0,0 +1,20 @@
+"""
+importataan User
+importataan database yhteys
+
+def testataan löytyykö käyttäjää
+"""
+class UserRepository:
+
+    """
+    yhteyden otto
+
+    palauta lista user-olioita
+
+    palauta käyttäjä käyttäjätunnuksen perusteella 
+    (palauttaa user-olion jos se on tietokannassa)
+
+    luo uusi käyttäjä (self,user), tallentaa käyttäjän user-oliona
+
+    poista kaikki käyttäjät
+    """
