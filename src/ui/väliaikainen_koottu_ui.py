6a2151674a8738397de34140b78d2804e1fa7a64
Susanna Mahlamäki <liisa.mahlamaki@helsinki.fi>
2022-11-25 18:39:26 +0200

    updating ui

--- src/ui/väliaikainen_koottu_ui.py
+++ src/ui/väliaikainen_koottu_ui.py
@@ -32,21 +32,21 @@ class UI:
         self._hide_current_view()
         
 
-        self._current_view = NewCardView(self._root,self._go_home)
+        self._current_view = NewCardView(self._root,self._go_home,self._show_edit_view)
         self._current_view.pack()
 
     def _show_edit_view(self):
         self._hide_current_view()
         
 
-        self._current_view = EditCardView(self._root)   
+        self._current_view = EditCardView(self._root, self._show_homepage_view)   
         self._current_view.pack() 
 
     def _show_practise_view(self):
         self._hide_current_view()
         
 
-        self._current_view = PractiseView(self._root)  
+        self._current_view = PractiseView(self._root, self._show_homepage_view)  
         self._current_view.pack()  
 
     
@@ -83,8 +83,8 @@ class HomePageView:
         
   
         create_card_button = ttk.Button(master=self._frame, text="Luo kortti", command=self._go_new_card)
-        edit_cards_button = ttk.Button(master=self._frame, text="Muokkaa kortteja", command=None)
-        practise_button = ttk.Button(master=self._frame, text="Kertaa", command=None)
+        edit_cards_button = ttk.Button(master=self._frame, text="Muokkaa kortteja", command=self._go_edit)
+        practise_button = ttk.Button(master=self._frame, text="Kertaa", command=self._go_practise)
         log_out_button = ttk.Button(master=self._frame, text="Lopeta", command=self._close)
 
         
@@ -96,16 +96,16 @@ class HomePageView:
        
 
 class NewCardView:
-    def __init__(self,root,go_home):
+    def __init__(self,root,go_home,card_list):
         self._root = root
         self._frame = None
         self._go_home = go_home
+        self._card_list = card_list
         self._question_entry = None
         self._answer_entry = None
         self._initialize()   
 
     def pack(self):
-        """"Näyttää näkymän."""
         self._frame.pack(fill=constants.X)
     
     def destroy(self):
@@ -118,7 +118,8 @@ class NewCardView:
         question_label = ttk.Label(master=self._frame, text = "Kirjoita kysymys")
         answer_label = ttk.Label(master=self._frame, text="Kirjoita vastaus")
         
-        back_home_button = ttk.Button(master=self._frame, text="Lopeta", command=self._go_home)
+        back_home_button = ttk.Button(master=self._frame, text="Takaisin", command=self._go_home)
+        save_button = ttk.Button(master=self._frame, text="Tallenna", command=self._card_list)
 
         
         self._question_entry=ttk.Entry(master=self._frame,width=40)
@@ -128,29 +129,44 @@ class NewCardView:
         self._question_entry.pack(padx=100, pady=100)
         answer_label.pack()
         self._answer_entry.pack(padx=100, pady=100)  
-        back_home_button.pack(side=constants.BOTTOM)
+        back_home_button.pack(side=constants.LEFT)
+        save_button.pack(side=constants.RIGHT)
+
         self._frame.grid_columnconfigure(0, weight=1)
 
 
 
 class EditCardView:
-    def __init__(self,root):
+    def __init__(self,root,go_home):
         self._root = root
         self._frame = None
+        self._go_home = go_home
         self._initialize()   
+
+    def pack(self):
+        self._frame.pack(fill=constants.X)
 
     def destroy(self):
         self._frame.destroy()
 
-
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
+
+        label = ttk.Label(master=self._frame, text = "Tähän tulee lista luoduista korteista,joita klikkaamalla pääsee muokkaamaan niitä tai poistamaan ne")
+        back_home_button = ttk.Button(master=self._frame, text="Takaisin", command=self._go_home)
+        label.pack()
+        back_home_button.pack(side=constants.LEFT)
+
 
 class PractiseView:
-    def __init__(self,root):
+    def __init__(self,root,go_home):
         self._root = root
         self._frame = None
+        self._go_home = go_home
         self._initialize()   
+
+    def pack(self):
+        self._frame.pack(fill=constants.X)
 
     def destroy(self):
         self._frame.destroy()
@@ -158,6 +174,12 @@ class PractiseView:
 
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
+        label = ttk.Label(master=self._frame, text = "Luultavasti pitää jakaa kolmeen osaan (näyttö kysymykselle, ")
+        label2 = ttk.Label(master=self._frame, text = "näyttö vastaukselle, ja näyttö että kysymykset ovat loppuneet")
+        back_home_button = ttk.Button(master=self._frame, text="Lopeta kertaus", command=self._go_home)
+        label.pack()
+        label2.pack()
+        back_home_button.pack(side=constants.LEFT)
 
 window = Tk()
 window.geometry("700x550")
